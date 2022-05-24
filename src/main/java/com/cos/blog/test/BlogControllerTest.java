6008b46451d911d549e8c3ad9072c9c9ce1f46ce
yangsomi11 <somi8945@naver.com>
2022-05-24 23:17:38 +0900

    MySQL 연동

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -1,14 +1,14 @@
 package com.cos.blog.test;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //특정 어노테이션이 붙어있는 클레스 파일을  new ioc -> 스프링 컨트롤러 관리
 public class BlogControllerTest {
-
+	
+	//http://localhost:8080/test/hello
 	@GetMapping("/test/hello")
 	public String hello() {
-		return "<h1>hello spring boot </h1>";
+		return "<h1>hello spring boot </h1>"; 
 	}
 }
