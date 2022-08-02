9d057d6b22f1c7150c9c61c693075ebf71150c91
yangsomi11 <somi8945@naver.com>
2022-08-02 22:54:45 +0900

    게시글 상세보기220802_양소미

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
@@ -23,6 +24,13 @@ public class BoardController {
 	public String index(Model model, @PageableDefault(size=3, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
 		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index"; //viewResolver 작동 
+	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board",boardService.글상세보기(id));
+		
+		return "board/detail";
 	}
 	
 	//USER권한이 필요
--- src/main/webapp/WEB-INF/views/board/saveForm.jsp
+++ src/main/webapp/WEB-INF/views/board/saveForm.jsp
@@ -8,8 +8,8 @@
 			<label for="title">Title</label> <input type="text" class="form-control" placeholder="Enter title" id="title">
 		</div>
 		<div class="form-group">
-			<label for="content:">Content:</label>
-			<textarea class="form-control summernote" rows="5" id="content:"></textarea>
+			<label for="content">Content:</label>
+			<textarea class="form-control summernote" rows="5" id="content"></textarea>
 		</div>
 	</form>
 	<button id="btn-save" class="btn btn-primary">글쓰기 완료</button>
