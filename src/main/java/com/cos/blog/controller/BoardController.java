7ee692a894c2944a4d48c2b9c23ec41bd81b8c27
yangsomi11 <somi8945@naver.com>
2022-08-08 15:18:51 +0900

    글 수정,삭제 기능 추가 작업 220808_양소미

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -33,6 +33,12 @@ public class BoardController {
 		return "board/detail";
 	}
 	
+	@GetMapping("/board/{id}/updateForm")
+	public String updateForm(@PathVariable int id, Model model) {
+		model.addAttribute("board",boardService.글상세보기(id));
+		return"board/updateForm";
+	}
+	
 	//USER권한이 필요
 	@GetMapping({"/board/saveForm"})  //@AuthenticationPrincipal PrincipalDetail principal
 	public String saveFrom() { //컨트롤러에서 세션을 어떻게 찾는가?
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,12 @@ let index = {
 		$("#btn-save").on("click", () => {	
 			this.save();
 		});
+		$("#btn-delete").on("click", () => {	
+			this.deleteById();
+		});
+		$("#btn-update").on("click", () => {	
+			this.update();
+		});
 	},
 
 
@@ -29,7 +35,52 @@ let index = {
 
 
 		});
-	}
+	},
+	
+	deleteById: function() {
+		let id = $("#id").text();
+		
+		$.ajax({
+			//회원가입 수행 요청
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json" 
+		}).done(function(resp) {
+			alert("삭제 완료되었습니다.");
+			console.log(resp);
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	update: function() {
+		let id =$("#id").val();
+		
+		//alert('user의 save 함수 호출');
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		console.log(id);
+		console.log(data);
+		$.ajax({
+			//회원가입 수행 요청
+			type: "PUT",
+			url: "/api/board/"+id,
+			data: JSON.stringify(data), 	
+			contentType: "application/json; charset=utf-8",
+			dataType: "json" 
+		}).done(function(resp) {
+			alert("글 수정이 완료되었습니다.");
+			console.log(resp);
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+
+
+		});
+	},
 }
 
 index.init();
